dad0ace584a987f67a2fc788338a7e1cd642c1fe
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-11-08 18:26:38 +0700

    Re-design movie results

--- src/Results/SuccessResult.js
+++ src/Results/SuccessResult.js
@@ -3,15 +3,15 @@ import "./Results.css";
 
 const SuccessResult = ({ results }) => {
   return (
-    <div className="results results-grid section-center">
+    <div className="results section-center">
       {results.map((item) => {
         const { Title, Poster, imdbID } = item;
         return (
           <Link to={`/movie/${imdbID}`} key={imdbID} className="item">
-            <article>
+            <div className="item__content">
               <img className="item__img" src={Poster} alt={Title} />
               <h4 className="item__title">{Title}</h4>
-            </article>
+            </div>
           </Link>
         );
       })}
